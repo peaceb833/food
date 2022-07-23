7b3d9dffbab67938828c01323f848f8f58c38f43
peaceb833 <68329632+peaceb833@users.noreply.github.com>
2022-07-23 03:46:36 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,34 @@
+
+btn= document.querySelector('.button1');
+cc= document.querySelector('.cc');
+cort= document.querySelector('.cort');
+
+
+//  cor=0
+// btn.addEventListener('click', function(){
+// 
+    // cor+=1
+// cc.innerText =cor
+// cc.classlist.add('cc')
+// 
+// 
+// })
+// 
+// 
+
+// cor=0
+// 
+// btn.addEventListener('click', function(){
+//    for (let i = 0; i<btn.length; i++){
+    // cor+=1
+    // cc.innerText =cor;
+    // cc.classlist.add('cc');
+    // return
+// 
+//    }
+//    console.log();
+// })
+
+cort.addEventListener('click', function(){
+    cort.classList.add('next')
+})
